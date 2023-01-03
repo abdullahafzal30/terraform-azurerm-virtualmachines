e21b8e461617bad1f991106e1cc83efcfe1f762b
Abdullah Afzal <118057454+abdullahafzal30@users.noreply.github.com>
2023-01-03 20:28:58 +0500

    first commit

--- geewa-terraform-azure-infrastructure-main/App Service/main.tf
+++ geewa-terraform-azure-infrastructure-main/App Service/main.tf
@@ -1,13 +1,13 @@
 
-resource "azurerm_resource_group" "rg-game-dev-tf-geewa-001" {
-  name     = "rg-game-dev-tf-geewa-001"
-  location = "West Europe"
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg
+  location = var.location
 }
 
-resource "azurerm_app_service_plan" "appserviceplan" {
-  name                = "appserviceplan"
-  location            = azurerm_resource_group.rg-game-dev-tf-geewa-001.location
-  resource_group_name = azurerm_resource_group.rg-game-dev-tf-geewa-001.name
+resource "azurerm_app_service_plan" "AppServicePlan" {
+  name                = "App-Service-Plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   sku {
     tier = "Standard"
@@ -16,16 +16,15 @@ resource "azurerm_app_service_plan" "appserviceplan" {
 }
 
 resource "azurerm_app_service" "app-service" {
-  name                = "app-service"
-  location            = azurerm_resource_group.rg-game-dev-tf-geewa-001.location
-  resource_group_name = azurerm_resource_group.rg-game-dev-tf-geewa-001.name
-  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+  name                = "App-Service"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.AppServicePlan.id
 
   site_config {
     dotnet_framework_version = "v4.0"
     scm_type                 = "LocalGit"
   }
-
   app_settings = {
     "SOME_KEY" = "some-value"
   }
